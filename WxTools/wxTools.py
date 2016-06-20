cc22470ffdb28ccd864db42314ddc540a450661d
Mercurial <taoanqi001@163.com>
2016-06-20 13:05:01 +0800

    完善token获取方法

--- WxTools/wxTools.py
+++ WxTools/wxTools.py
@@ -6,6 +6,11 @@ Created on 2016年6月20日
 '''
 import urllib,urllib2
 
-class wxTool():
+class wxTools():
     def getToken(self,appid='',appsceret=''):
-        
+        if False:
+            pass
+        else:
+            wxParams=urllib.urlencode({"grant_tpye":"client_credential","appid":appid,"secret":appsceret})
+            f=urllib.urlopen("https://api.weixin.qq.com/cgi-bin/token?%s"%wxParams)
+            
