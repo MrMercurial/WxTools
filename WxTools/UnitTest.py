f2318dd252abc7ba8ddd119eb9518400834c6bd2
Mercurial <taoanqi001@163.com>
2016-06-24 12:59:26 +0800

    同步不同电脑上的代码

---
+++ WxTools/UnitTest.py
@@ -0,0 +1,35 @@
+#encoding=utf-8
+'''
+Created on 2016年6月21日
+
+@author: Ant
+'''
+from wxTools import wxTools
+import urllib,urllib2
+import json
+
+appid='wxbf844f771df2017d'
+secret='d4624c36b6795d1d99dcf0547af5443d'
+wxtest=wxTools(appid,secret)
+
+token=wxtest.getToken(appid=appid,appsecret=secret)
+p=urllib.urlencode({"access_token":token})
+
+#########################################
+##消息推送测试###########################
+# c=json.dumps({
+#    "filter":{
+#         "is_to_all":True,
+#         "group_id":0
+#    },
+#    "text":{
+#       "content":"CONTENT"
+#    },
+#     "msgtype":"text"
+# })
+# f=urllib.urlopen('https://api.weixin.qq.com/cgi-bin/message/mass/sendall?%s'%p,c)
+# print f.read()
+#########################################
+
+##################测试图片上传#######################
+
