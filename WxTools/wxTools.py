ef7990ac8433aa724cbb2d16649d4535fbda4270
Mercurial <taoanqi001@163.com>
2016-06-24 12:48:27 +0800

    同步不同电脑上的代码

--- WxTools/wxTools.py
+++ WxTools/wxTools.py
@@ -5,12 +5,41 @@ Created on 2016年6月20日
 @author: Ant
 '''
 import urllib,urllib2
+import json
+#from _mysql import NULL
 
 class wxTools():
-    def getToken(self,appid='',appsceret=''):
+    def __init__(self,appid,appsecret):
+        self.wxid=appid
+        self.wxsecret=appsecret
+    
+    #获取token方法
+    #token获取默认以实例属性作为参数
+    def getToken(self,appid='',appsecret=''):
+        if appid=='':
+            appid=self.wxid
+        if appsecret=='':
+            appsecret=self.wxsecret
+        
         if False:
             pass
         else:
-            wxParams=urllib.urlencode({"grant_tpye":"client_credential","appid":appid,"secret":appsceret})
+            wxParams=urllib.urlencode({"grant_type":'client_credential',"appid":appid,"secret":appsecret})
             f=urllib.urlopen("https://api.weixin.qq.com/cgi-bin/token?%s"%wxParams)
-            
+            wxToken=json.loads(f.read())
+            try:
+                #print wxToken
+                return wxToken["access_token"]
+            except:
+                return "token获取失败"
+            #return wxToken
+    
+    #菜单设置
+    def setMenu(self,token):
+        pass
+    
+    #消息发送
+    #
+    def sendMessage(self,token):
+        pass
+           
