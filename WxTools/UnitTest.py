67fab1e485784f28668c8e8b948d8ec9ac332631
Mercurial <taoanqi001@163.com>
2016-06-30 14:02:15 +0800

    测试上传，未完成

--- WxTools/UnitTest.py
+++ WxTools/UnitTest.py
@@ -7,6 +7,7 @@ Created on 2016年6月21日
 from wxTools import wxTools
 import urllib,urllib2
 import json
+import pycurl
 
 appid='wxbf844f771df2017d'
 secret='d4624c36b6795d1d99dcf0547af5443d'
@@ -14,7 +15,7 @@ wxtest=wxTools(appid,secret)
 
 token=wxtest.getToken(appid=appid,appsecret=secret)
 p=urllib.urlencode({"access_token":token})
-
+print token
 #########################################
 ##消息推送测试###########################
 # c=json.dumps({
@@ -36,8 +37,23 @@ p=urllib.urlencode({"access_token":token})
 # print('第二次')
 # print('test')
 
-pvar=urllib.urlencode({"access_token":token,"type":"image"})
-c={'media':{'filename':'/home/ant/01.jpg','filelength':'','content-type':'image/jpeg'}}
-t=urllib.urlencode(c)
-f=urllib.urlopen('https://api.weixin.qq.com/cgi-bin/media/upload?%s'%pvar,t)
-print f.read()
+# pvar=urllib.urlencode({"access_token":token,"type":"image"})
+# c={'media':{'filename':'/home/ant/01.jpg','Content-length':'940','Content-Type':'image/jpeg'}}
+# # c={'media':r'D:\01.jpg'}
+# data={'file':()}
+# t=urllib.urlencode(c)
+# request= urllib2.Request('https://api.weixin.qq.com/cgi-bin/media/upload?%s'%pvar,t)
+# f=urllib2.urlopen(request)
+# #f=urllib.urlopen('https://api.weixin.qq.com/cgi-bin/media/upload?%s'%pvar,t)
+c=pycurl.Curl()
+url="https://api.weixin.qq.com/cgi-bin/media/upload?access_token=%s&type=%s"%(token,"image")
+c.setopt(c.URL,url)
+c.setopt(c.SSL_VERIFYPEER,False)
+c.setopt(c.SSL_VERIFYHOST,False)
+c.setopt(c.CUSTOMREQUEST,"POST")
+c.setopt(c.POST,1)
+c.setopt(c.POSTFIELDS,urllib.urlencode({"media":'D:\01.jpg'}))
+f=c.perform()
+
+print f
+# print f.read()
